3683d634877bb38f62336c5afeeebd0f58457f44
monik <monik@vaishumoni>
2025-07-22 11:16:31 +0530

    this my first customer java program

---
+++ C2CT_CoreJavaProgram/src/Day3/Entity/CustomerDemo.java
@@ -0,0 +1,25 @@
+package Day3.Entity;
+
+public class CustomerDemo {
+
+
+		public static void main(String[] args) {
+			
+			Customer c1=new Customer();
+			c1.setCid(101);
+			c1.setName("Raja");
+			c1.setCity("chennai");
+			System.out.println(c1);
+			
+			Customer c2=new Customer();
+			c2.setCid(102);
+			c2.setName("Priya");
+			c2.setCity("Puducherry");
+			System.out.println(c2);
+
+		}
+
+		
+
+	}
+
