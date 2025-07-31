4916bf5be95c1e1279d9aa353e2232515a71fccd
monik <monik@vaishumoni>
2025-07-31 14:47:25 +0530

    this is my Exception handling java program

---
+++ C2CT_CoreJavaProgram/src/Day9/StringSubClass.java
@@ -0,0 +1,13 @@
+package Day9;
+
+public class StringSubClass {
+	package com.tnsif.daynine;
+
+	public class StringSubClass extends String {
+
+		public static void main(String[] args) {
+			
+
+		}
+	}
+
