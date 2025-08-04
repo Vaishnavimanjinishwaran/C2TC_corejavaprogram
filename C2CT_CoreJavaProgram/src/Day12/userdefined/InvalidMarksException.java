19c4dc873cb5be6f2d5d8a9a178eb0210e845c04
monik <monik@vaishumoni>
2025-08-04 14:03:51 +0530

    this is my thread java program

---
+++ C2CT_CoreJavaProgram/src/Day12/userdefined/InvalidMarksException.java
@@ -0,0 +1,15 @@
+package Day12.userdefined;
+
+	public class InvalidMarksException extends Exception{
+
+		public InvalidMarksException() {
+			super();
+		}
+
+		public InvalidMarksException(String message) {
+			super(message);
+		}
+
+		
+	}
+
